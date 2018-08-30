d69a71a4e535046d753c5654af6ad1d4642989bf
kperkins2288 <kperkins2288@gmail.com>
2018-08-30 18:09:39 -0400

    Update 1, initial build

---
+++ Engine/Models/Player.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Engine.Models
+{
+    public class Player : INotifyPropertyChanged
+    {
+        private string _name;
+        private string _characterClass;
+        private int _health;
+        private int _exp;
+        private int _level;
+        private int _gold;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string CharacterClass
+        {
+            get { return _characterClass; }
+            set
+            {
+                _characterClass = value;
+                OnPropertyChanged("Class");
+            }
+        }
+        public int Health
+        {
+            get { return _health; }
+            set
+            {
+                _health = value;
+                OnPropertyChanged("Health");
+            }
+        }
+        public int Exp
+        {
+            get { return _exp; }
+            set
+            {
+                _exp = value;
+                OnPropertyChanged("Exp");
+            }
+        }
+        public int Level
+        {
+            get { return _level; }
+            set
+            {
+                _level = value;
+                OnPropertyChanged("Level");
+            }
+        }
+        public int Gold
+        {
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged("Gold");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged (string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ Engine/ViewModels/GameSession.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.ViewModels
+{
+    public class GameSession
+    {
+        public Player CurrentPlayer { get; set; }
+        public Location CurrentLocation { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player();
+            CurrentPlayer.Name = "Gob";
+            CurrentPlayer.CharacterClass = "Warrior";
+            CurrentPlayer.Health = 20;
+            CurrentPlayer.Gold = 1000;
+            CurrentPlayer.Exp = 0;
+            CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.Xcoord = 0;
+            CurrentLocation.Ycoord = -1;
+            CurrentLocation.Description = "Home sweet home";
+        }
+    }
+}
